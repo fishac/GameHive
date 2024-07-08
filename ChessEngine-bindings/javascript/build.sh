5d0b5eeb8e2b268f9f698f144909463036db2068
Alex Fish <acfishx@gmail.com>
2024-07-07 19:49:28 -0500

    Clean js builds

--- ChessEngine-bindings/javascript/build.sh
+++ ChessEngine-bindings/javascript/build.sh
@@ -1,9 +1,11 @@
 #!/bin/ash
 
-mkdir build
+mkdir -p build/lib
+mkdir build/src
 cd build
-mkdir lib
 cp ../../../ChessCore ./lib -r
 cp ../../../ChessEngine ./lib -r
-docker run --rm -v $(pwd)/..:/src -u $(id -u):$(id -g) emscripten/emsdk emcmake cmake
-docker run --rm -v $(pwd)/..:/src -u $(id -u):$(id -g) emscripten/emsdk make 
+cp ../src/*.cpp ./src
+cp ../CMakeLists.txt .
+docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk emcmake cmake
+docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk make 
