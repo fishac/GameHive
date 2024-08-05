5b59ca44483e73c4b95c08582fdd0b1fe76b1c6b
fishac <acfishx@gmail.com>
2024-08-04 19:42:18 -0500

    More tests

--- ChessEngine-bindings/javascript/test/ChessEngineJS.test.js
+++ ChessEngine-bindings/javascript/test/ChessEngineJS.test.js
@@ -120,9 +120,90 @@ test('BoardState functionality', () => {
 		expect(bs.getLegalMoves()).toBeTruthy();
 		expect(typeof bs.getLegalMoves()).toBe("object");
 		expect(bs.getLegalMoves().size()).toBe(20);
-		const legalMovesArray = new Array(bs.getLegalMoves().size()).fill(0).map((_, id) => bs.getLegalMoves().get(id));
-		expect(legalMovesArray.some(m => (
+		
+		expect(bs.getTurnColor()).toBe(true);
+		const legalMovesArrayW = new Array(bs.getLegalMoves().size()).fill(0).map((_, id) => bs.getLegalMoves().get(id));
+		expect(legalMovesArrayW.length).toBe(20);
+		expect(legalMovesArrayW.some(m => (
 			m.from === 8 && m.to === 16 && m.promotionPiece === 0
 		))).toBe(true);
+		
+		const legalMovesFromB1Array = new Array(bs.getLegalMovesFromSquare(1).size()).fill(0).map((_, id) => bs.getLegalMovesFromSquare(1).get(id));
+		expect(legalMovesFromB1Array.length).toBe(2);
+		expect(legalMovesFromB1Array.some(m => (
+			m.from === 1 && m.to === 16 && m.promotionPiece === 0
+		))).toBe(true);
+		
+		expect(bs.makeMove({from: 16, to: 17, promotionPiece: 1})).toBe(false);
+		expect(bs.makeMove({from: 1, to: 16, promotionPiece: 0})).toBe(true);
+		expect(bs.getTurnColor()).toBe(false);
+		
+		const legalMovesArrayB = new Array(bs.getLegalMoves().size()).fill(0).map((_, id) => bs.getLegalMoves().get(id));
+		expect(legalMovesArrayB.length).toBe(20);
+		expect(legalMovesArrayB.some(m => (
+			m.from === 62 && m.to === 47 && m.promotionPiece === 0
+		))).toBe(true);
+		
+		const legalMovesFromG8Array = new Array(bs.getLegalMovesFromSquare(62).size()).fill(0).map((_, id) => bs.getLegalMovesFromSquare(62).get(id));
+		expect(legalMovesFromG8Array.length).toBe(2);
+		expect(legalMovesFromG8Array.some(m => (
+			m.from === 62 && m.to === 47 && m.promotionPiece === 0
+		))).toBe(true);
+		
+		expect(bs.makeMove({from: 16, to: 17, promotionPiece: 1})).toBe(false);
+		expect(bs.makeMove({from: 62, to: 47, promotionPiece: 0})).toBe(true);
+		
+		expect(bs.isMoveLegal({from: 16, to: 17, promotionPiece: 1})).toBe(false);
+		expect(bs.isMoveLegal({from: 6, to: 15, promotionPiece: 0})).toBe(true);
+		
+		expect(bs.getCheckStatus()).toBe(false);
+		expect(bs.getCheckmateStatus()).toBe(false);
+		expect(bs.getStalemateStatus()).toBe(false);
+		expect(bs.isDrawByRepitition()).toBe(false);
+		expect(bs.isDrawByInsufficientMaterial()).toBe(false);
+		expect(bs.getTurnColor()).toBe(true);
+		expect(bs.getWhiteKingSquare()).toBe(4);
+		expect(bs.getBlackKingSquare()).toBe(60);
+		
+		expect(bs.getPieceOnSquare(0)).toBe(4);
+		expect(bs.getPieceOnSquare(1)).toBe(0);
+		expect(bs.getPieceOnSquare(2)).toBe(3);
+		expect(bs.getPieceOnSquare(3)).toBe(5);
+		expect(bs.getPieceOnSquare(4)).toBe(6);
+		expect(bs.getPieceOnSquare(8)).toBe(1);
+		expect(bs.getPieceOnSquare(16)).toBe(2);
+		
+		expect(bs.getPieceOnSquare(63)).toBe(4);
+		expect(bs.getPieceOnSquare(62)).toBe(0);
+		expect(bs.getPieceOnSquare(61)).toBe(3);
+		expect(bs.getPieceOnSquare(60)).toBe(6);
+		expect(bs.getPieceOnSquare(59)).toBe(5);
+		expect(bs.getPieceOnSquare(55)).toBe(1);
+		expect(bs.getPieceOnSquare(47)).toBe(2);
+		
+		expect(bs.getColorOnSquare(0)).toBe(true);
+		expect(bs.getColorOnSquare(2)).toBe(true);
+		expect(bs.getColorOnSquare(3)).toBe(true);
+		expect(bs.getColorOnSquare(4)).toBe(true);
+		expect(bs.getColorOnSquare(8)).toBe(true);
+		expect(bs.getColorOnSquare(16)).toBe(true);
+		
+		expect(bs.getColorOnSquare(63)).toBe(false);
+		expect(bs.getColorOnSquare(61)).toBe(false);
+		expect(bs.getColorOnSquare(60)).toBe(false);
+		expect(bs.getColorOnSquare(59)).toBe(false);
+		expect(bs.getColorOnSquare(55)).toBe(false);
+		expect(bs.getColorOnSquare(47)).toBe(false);
+		
+		expect(bs.isSquareOccupied(0)).toBe(true);
+		expect(bs.isSquareOccupied(1)).toBe(false);
+		expect(bs.isSquareOccupied(63)).toBe(true);
+		expect(bs.isSquareOccupied(62)).toBe(false);
+	});
+	
+test('BoardState functionality', () => {
+	return ChessEngineJS().then(ce => {
+		const eng = new ce.ChessEngine();
+		
 	});
 });
