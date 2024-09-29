6a9ce667de38582b496d86fa239c8b89c661612f
Alex Fish <acfishx@gmail.com>
2024-09-28 22:00:03 -0500

    Fix timeout

--- ChessEngine-bindings/javascript/buildforweb.sh
+++ ChessEngine-bindings/javascript/buildforweb.sh
@@ -7,3 +7,4 @@ cp ../src/*.cpp ./src
 cp ../CMakeLists.txt .
 docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk emcmake cmake -DBUILD_FOR_WEB=1
 docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk make 
+
--- prepare_docker_build.sh
+++ prepare_docker_build.sh
@@ -1,2 +1,5 @@
 cd ./ChessEngine-bindings/javascript
 ./buildforweb.sh
+
+cd -
+cp ./ChessEngine-bindings/javascript/build/ChessEngineJS.js ./GameHive-web/app/lib/ChessEngineJS.js
